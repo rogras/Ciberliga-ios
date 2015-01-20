befd6f0f12431d7e5e796f9d1ef4e7f002ed44fb
Roger Gras <xeli82@gmail.com>
2015-01-20 17:54:37 +0100

    project structure

---
+++ Ciberliga/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ciberliga
+//
+//  Created by Roger Gras on 20/1/15.
+//  Copyright (c) 2015 rogras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '6.0'
+pod 'Parse'
+pod 'ParseFacebookUtils'
+pod 'JASidePanels'
+pod 'AFNetworking', '~> 2.0'
